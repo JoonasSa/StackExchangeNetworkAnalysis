fe2f4f679f3071b64ef6398e9fa7825913e58a5c
Rafael <rafael.leal@runbox.com>
2019-02-12 23:07:03 +0200

    Starting to build the new network. Does not work yet.
    Maybe the structure is too convoluted?

--- database_handler.py
+++ database_handler.py
@@ -8,13 +8,31 @@ import json
 from data_reader import get_stackoverflow_data
 
 
-def __extract_users(posts_xml):
+def __extract_user_count(posts_xml):
     '''Counts how many times each user id has posted a question or answer'''
     user_counter = defaultdict(int)
     for post in posts_xml:
         user_id = post.attrib.get('OwnerUserId', post.attrib.get('OwnerDisplayName'))
         user_counter[user_id] += 1
     return user_counter
+
+def __extract_answers_as_edges(posts_xml):
+    posts_dict = dict()
+    edges = defaultdict(list)
+    for row in posts_xml:
+        user_id = row.attrib.get('OwnerUserId', row.attrib.get('OwnerDisplayName'))
+        # PostTypeId == 1 indicates that it's a question
+        if row.attrib['PostTypeId'] == '1':
+            posts_dict[row.attrib['Id']] = user_id
+        # otherwise it's an answer to an existing question
+        elif row.attrib['PostTypeId'] == '2':
+            # fetches the id of the OP and links them
+            parent_user_id = posts_dict.get(row.attrib['ParentId'])
+            creation_date = row.attrib.get('CreationDate')
+            if not user_id or not parent_user_id:
+                continue
+            edges[','.join((user_id, parent_user_id))].append(creation_date)
+    return edges
 
 
 def __dump_json(data, filepath):
@@ -23,15 +41,18 @@ def __dump_json(data, filepath):
         json.dump(data, outfile)
 
 
-def remove_user_json(path, filename='users.json'):
-    '''Removes the json database. If necessary'''
+def __remove_user_json(path, filename='users.json'):
+    '''Removes the json database'''
     with suppress('FileNotFoundError'):
         for dirname in os.listdir(path):
             os.remove(os.path.join(path, dirname, filename))
 
 
-def prepare_json_databases(path, filename='users.json'):
-    '''Per-directory handling of data: requests feature extraction and saving'''
+def prepare_json_databasis(path, function, filename, reset=False):
+    '''Per-directory handling of data: requests feature extraction and saving
+       Resets databaset if _reset_ is True'''
+    if reset:
+        __remove_user_json(path, filename)
     for dirname in os.listdir(path):
         print('Extracting data from', dirname)
         dirpath = os.path.join(path, dirname)
@@ -40,8 +61,8 @@ def prepare_json_databases(path, filename='users.json'):
         if os.path.isfile(filepath_json):
             continue
         xml_data = get_stackoverflow_data(dirpath, ['Posts'])
-        user_counter = __extract_users(xml_data['Posts'])
-        __dump_json(user_counter, filepath_json)
+        network = function(xml_data['Posts'])
+        __dump_json(network, filepath_json)
 
 
 def load_json_database(path, dirname, filename='users.json'):
--- main.py
+++ main.py
@@ -8,39 +8,22 @@ import matplotlib.pyplot as plt
 import scipy
 import os
 import json
-import database_handler
+import network_creator
 #from data_reader import get_stackoverflow_data
 
 BASE_PATH = 'data'
 
-G=nx.Graph()
+#G=nx.Graph()
+#g_network = network_creator.page_and_crossposters_network(BASE_PATH)
+#G.add_weighted_edges_from(g_network['edges'])
+#G.add_nodes_from(g_network['nodes'])
+## Drawing edge thickness according to width
+#plt.figure(1, figsize=(16,16))
+#raw_weights = [w/500 for w in nx.get_edge_attributes(G, 'weight').values()]
+#max_weight = max(raw_weights)
+#weights = [w*10/max_weight for w in raw_weights]
+#nx.draw(G, with_labels=True, node_color='skyblue', width=weights)
+#plt.show()
 
-#xml_dict = get_stackoverflow_data(f"{BASE_PATH}/bitcoin")
-
-# example on how to use the xml
-#for row in xml_dict['Users'][:2]:
-#    # row objects have tag & attrib
-#    print(row.tag, row.attrib)
-
-# Extraction of features into individual json files for each directory
-database_handler.prepare_json_databases(BASE_PATH)
-
-G.add_nodes_from(os.listdir(BASE_PATH))
-
-edges = []
-# Each directory represents a site
-for dir1, dir2 in combinations(os.listdir(BASE_PATH), 2):
-    users1 = database_handler.load_json_database(BASE_PATH, dir1)
-    users2 = database_handler.load_json_database(BASE_PATH, dir2)
-    # The weight of the edge is the number of users that have posted in both sites
-    weight = len(set(users1) & set(users2))
-    edges.append((dir1, dir2, weight))
-G.add_weighted_edges_from(edges)
-
-# Drawing edge thickness according to width
-plt.figure(1, figsize=(16,16))
-raw_weights = [w/500 for w in nx.get_edge_attributes(G, 'weight').values()]
-max_weight = max(raw_weights)
-weights = [w*10/max_weight for w in raw_weights]
-nx.draw(G, with_labels=True, node_color='skyblue', width=weights)
-plt.show()
+#H=nx.Graph()
+#h_network = network_creator.answers_and_posters_network(BASE_PATH)
